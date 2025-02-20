e245fe644063734a11b63219db478d04afabb0a5
Абиняков <danabinar@gamil.com>
2025-02-20 22:53:43 +0300

    Обновление файлов

--- Work0.php
+++ Work0.php
@@ -1,10 +1,12 @@
 <?php
 $x = 1;
-echo "Введите конечное число ";
-$n = readline() . PHP_EOL;
+echo "Введите конечное число: ";
+$n = (int) readline();
+
 while ($x <= $n) {
-   if ($x % 3 == 0) {
-      echo $x . PHP_EOL;
-   }
-   ++$x;
+    if ($x % 3 === 0) {
+        echo $x . PHP_EOL;
+    }
+    ++$x;
 }
+
--- Work2.php
+++ Work2.php
@@ -1,12 +1,13 @@
 <?php
 for ($num = 1; $num <= 100; $num++) {
-   $norml = true;
-   for ($i = 2; $i <= sqrt($num); $i++) {
-      if ($num % $i == 0) {
-         $norml = false;
-      }
-   }
-   if ($norml && $num > 1) {
-      echo $num . PHP_EOL;
-   }
+    $norml = true;
+    for ($i = 2; $i <= sqrt($num); $i++) {
+        if ($num % $i === 0) { 
+            $norml = false;
+        }
+    }
+    if ($norml && $num > 1) { 
+        echo $num . PHP_EOL;
+    }
 }
+
